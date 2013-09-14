284e1b5df1bbedf3d95125208494b4f751679857
Chedli Bourguiba <bourguiba.chedli@gmail.com>
2013-09-15 01:18:29 +0200

    Remove unnecessary signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      sign_in @user
+      #sign_in @user
       redirect_to @user
     else
       render 'edit'
